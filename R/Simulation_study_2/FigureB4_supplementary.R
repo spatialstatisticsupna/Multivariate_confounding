90a7f77df832a539ab5eb78b106d797910f36d62
ArantxaUrdangarin <42638250+ArantxaUrdangarin@users.noreply.github.com>
2023-11-16 15:05:28 +0100

    Add files via upload

---
+++ R/Simulation_study_2/FigureB4_supplementary.R
@@ -0,0 +1,66 @@
+
+rm(list=ls())
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+
+# Load packages
+library(sf)
+library(RColorBrewer)
+library(tmap)
+library(latex2exp)
+
+
+# Define the scenario 
+scenario <- "Scenario3"
+# scenario <- "Scenario4"
+# scenario <- "Scenario5"
+
+
+
+#################
+# Load the data #
+#################
+load(paste0("Simulated_data/SimuStudy2_", scenario, ".Rdata"))
+head(data)
+
+
+S <- length(unique(data$dist))
+
+
+
+##################################################################
+# Figure 3: plot simulated covariate X1* and the spatial effects #
+##################################################################
+carto_UP$X1.aux <- data$X1.aux[1:S]
+carto_UP$xi.crime1 <- data$xi[1:S]
+carto_UP$xi.crime2 <- data$xi[(S+1):(2*S)]
+
+
+
+paleta <- brewer.pal(9,"YlOrRd")
+values <- c(-3.21,-0.8,-0.5,-0.25,0,0.25,0.5,0.8, 1.5,3.27)
+
+
+tmap_mode("plot")
+plot <- tm_shape(carto_UP) +
+  tm_polygons(col=c("X1.aux", "xi.crime1", "xi.crime2"), palette=paleta, title="", legend.show=T,
+              legend.reverse=T, style="fixed", breaks=values, interval.closure="left", border.col="black") +
+  tm_layout(main.title="", main.title.position="center", legend.text.size=1,
+            panel.labels=c("X1*",TeX('$\\bf{Theta}_{1}$'), TeX('$\\Theta_{2}$')), panel.label.bg.color="lightskyblue",
+            legend.outside=T, legend.outside.position="right", legend.frame=F, outer.margins=0) +
+  tm_facets(ncol=3, nrow=1)
+
+print(plot)
+
+
+if (scenario=="Scenario3"){
+  tmap_save(plot, file="FigureB4a.pdf", width=12, height=4)
+}
+if (scenario=="Scenario4"){
+  tmap_save(plot, file="FigureB4b.pdf", width=12, height=4)
+}
+if (scenario=="Scenario5"){
+  tmap_save(plot, file="FigureB4c.pdf", width=12, height=4)
+}
+
+
+
---
+++ R/Simulation_study_2/run_MBYM2_Spat.R
@@ -0,0 +1,83 @@
+
+################################################################################
+#                           Spatial M-models: MBYM2                            #
+################################################################################
+
+rm(list=ls())
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+
+# Load packages
+library(INLA)
+inla.setOption(inla.mode="classic")
+library(spdep)
+library(spatialreg)
+library(fastDummies)
+library(sf)
+
+
+n.sim <- 300
+
+# Define the scenario
+scenario <- "Scenario1"
+# scenario <- "Scenario2"
+# scenario <- "Scenario3"
+# scenario <- "Scenario4"
+# scenario <- "Scenario5"
+
+
+
+#######################
+# Load simulated data #
+#######################
+load(paste0("Simulated_data/SimuStudy2_", scenario, ".Rdata"))
+
+
+J <- length(unique(data$Crime))
+S <- length(unique(data$dist))
+
+
+
+##########################
+# Load M-model functions #
+##########################
+source("functions/mbym2_bartlett.R") 
+source("functions/MCAR_single_covariate.R") 
+
+
+
+###################################
+# Define spatial structure matrix #
+###################################
+nb.mat <- nb2mat(poly2nb(carto_UP), style ="B")
+nbs <- unlist(lapply(poly2nb(carto_UP), function(x) length(x)))
+
+Q.xi <- diag(nbs)-nb.mat
+
+
+
+#######################################
+# Multivariate ICAR models: M-Spatial #
+#######################################
+
+# Folder to save results
+if(!file.exists(paste0("results_", scenario)))
+      dir.create(paste0("results_", scenario))
+
+
+for (i in 1:n.sim){
+  print(paste0("i=", i))
+  micar.spat <- NULL
+  # Simulated counts
+  data$obs <- simu.O[[i]]
+  # Fit the model
+  mbym2.spat <- MCAR.Covariate(carto=carto_UP, data=data, ID.area="dist", ID.disease="Crime",
+                               O="obs", E="exp", covariate="X1.aux", spatial="bym2.bartlett",
+                              strategy="simplified.laplace")
+  
+  data$obs <- NULL
+
+  save(mbym2.spat, file=paste0("results_", scenario, "/MBYM2_Spat_",i,".Rdata"))
+}
+
+
+
